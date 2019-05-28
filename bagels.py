a0d4d37f8528253df63d60defc67745f5b2b3664
Taylor Cooke <taylorcooke@gmail.com>
2019-05-28 09:46:25 -0400

    initial function

---
+++ bagels.py
@@ -0,0 +1,12 @@
+import random
+
+NUM_DIGITS = 3
+MAX_GUESS = 10
+
+def getSecretNum():
+    numbers = list(range(10))
+    random.shuffle(numbers)
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
