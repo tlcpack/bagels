511fac5c628ed399370b9ebfb7709856c54b2c13
Taylor Cooke <taylorcooke@gmail.com>
2019-05-28 09:56:15 -0400

    functions created

--- bagels.py
+++ bagels.py
@@ -10,3 +10,29 @@ def getSecretNum():
     for i in range(NUM_DIGITS):
         secretNum += str(numbers[i])
     return secretNum
+
+def getClues(guess, secretNum):
+    if guess == secretNum:
+        return 'You got it!'
+
+    clues[]
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            clues.append('Pico')
+    if len(clues) == 0:
+        return 'Bagels'
+
+    clues.sort()
+    return ' '.join(clues)
+
+def isOnlyDigits(num):
+    if num =='':
+        return False
+    for i in num:
+        if i not in '0 1 2 3 4 5 6 7 8 9'.split():
+            return False
+    
+    return True
+
